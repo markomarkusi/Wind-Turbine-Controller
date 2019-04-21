7c6770873d2354cf145f80c414147602afcfa85e
markomarkusi <marko.markusi@hotmail.com>
2019-04-21 09:25:48 -0700

    Fixed ADC sampling rate to match minimum sampling rate

--- Src/adc.c
+++ Src/adc.c
@@ -142,7 +142,7 @@ void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_6;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
+  sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
